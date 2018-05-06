890e0f6a7d4ec75decc8810535d1f2beeb0de085
greg <greg100795@gmail.com>
2018-05-05 20:50:02 -0400

    Timeout implemented

--- Assets/PlayVideo.cs
+++ Assets/PlayVideo.cs
@@ -6,15 +6,50 @@ using UnityEngine.UI;
 
 
 public class PlayVideo : MonoBehaviour {
+    public float vidTimer;
+    public GameObject MainMenuPanel;
+    public GameObject EventsPanel;
+    public GameObject DropinPanel;
+    public GameObject MapPanel;
+    public GameObject AttractLoopPanel;
+    public GameObject Background;
 
+    // Use this for initialization
+    void Start () {
+        //initalizing timer value
+        vidTimer = 30.0f;
+        //retrieve objects
+        MainMenuPanel = GameObject.Find("MainMenuPanel");
+        DropinPanel = GameObject.Find("DropinPanel");
+        MapPanel = GameObject.Find("MapPanel");
+        EventsPanel = GameObject.Find("EventsPanel");
+        AttractLoopPanel = GameObject.Find("AttractLoopPanel");
+        Background = GameObject.Find("Background");
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+        AttractLoopPanel.SetActive(true);
+        MainMenuPanel.SetActive(false);
+        DropinPanel.SetActive(false);
+        MapPanel.SetActive(false);
+        EventsPanel.SetActive(false);
+        Background.SetActive(false);
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        vidTimer -= Time.deltaTime;
+        if (Input.anyKey)
+            vidTimer = 30.0f;
+        if (vidTimer < 0)
+        {
+            AttractLoopPanel.SetActive(true);
+            MainMenuPanel.SetActive(false);
+            DropinPanel.SetActive(false);
+            MapPanel.SetActive(false);
+            EventsPanel.SetActive(false);
+            Background.SetActive(false);
+            vidTimer = 25.0f;
+
+        }
+    }
 }
